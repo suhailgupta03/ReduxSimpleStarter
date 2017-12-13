de4deded73e6215b7b2e8af7f7b0ec25ebe78236
Suhail <suhailgupta03@gmail.com>
2017-12-13 22:41:47 +0530

    more changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 // Create a component. This component should produce some HTML
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyD2JD-HH2L1muyuk3l9pwuv4dp2YtwfzwY";
 
-const App =  () => {
-    return <div>
-        <SearchBar />
-    </div>;
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, (videos) => {
+            this.setState({ videos, selectedVideo: videos[0] });
+        });
+    }
+    render() {
+        return <div>
+            <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+        </div>;
+    }
 }
 
 
